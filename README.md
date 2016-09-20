c5afe5298427dc7504219b410dca7366df8d4801
June Domingo <june@cynder.io>
2016-09-20 20:51:50 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+### React Native Rename
+Rename react-native app with just one command
+
+>This package assumes that your project `name` in `package.json` and registered component in AppRegistry are the same.
+
+#### Installation
+```
+npm install react-native-rename -g
+```
+
+#### Usage
+```
+react-native-rename CURRENT_NAME NEW_NAME
+```
+
+`CURRENT_NAME` is from `package.json` name property
---
+++ index.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node --harmony
+/* eslint-disable no-console */
+const childProcess = require('child_process');
+const name = require('project-name');
+const program = require('commander');
+const replace = require("replace");
+
+const projectName = name();
+
+program
+	.version('1.0.0')
+	.arguments('<currentName> <newName>')
+	.action((currentName, newName) => {
+		if (currentName !== projectName) return console.log("App name didn't match");
+
+		const pattern = /^([0-9]|[a-z])+([0-9a-z]+)$/i;
+
+		if (!pattern.test(newName)) {
+			return console.log(`${newName} is not a valid name for a project. Please use a valid identifier name (alphanumeric).`);
+		}
+
+		replace({
+			regex: currentName,
+			replacement: newName,
+			paths: [
+				'./index.ios.js',
+				'./index.android.js',
+				'./android/settings.gradle',
+				'./android/app/src/main/res/values/strings.xml',
+				'./package.json'
+			],
+			recursive: true,
+			silent: true
+		});
+
+		// Lowercase strings
+		replace({
+			regex: currentName.toLowerCase(),
+			replacement: newName.toLowerCase(),
+			paths: [
+				'./android/app/BUCK',
+				'./android/app/build.gradle',
+				'./android/app/src/main/AndroidManifest.xml'
+			],
+			recursive: true,
+			silent: true
+		});
+
+		// Delete folders
+		const androidFolder = currentName.toLowerCase();
+		childProcess.exec(`rm -rf ios/build ios/${currentName} ios/${currentName}.xcodeproj ios/${currentName}Tests android/app/src/main/java/com/${androidFolder}`, (error, stdout) => {
+			if (error !== null) console.log(`exec error: ${error}`);
+		});
+
+		// Upgrade app
+		childProcess.exec('react-native upgrade', (error, stdout) => {
+			if (error !== null) console.log(`exec error: ${error}`);
+			console.log(`App successfully renamed to ${newName}`);
+		});
+	})
+	.parse(process.argv);
+
+if (!process.argv.slice(2).length) program.outputHelp();
