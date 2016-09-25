48053f5c5e3b6d27e5b51d54e71522098ac69bf2
June Domingo <june@cynder.io>
2016-09-26 02:15:14 +0800

    Changed project name reference; placed 'renaming folders' code on top; Change the way how to reaname ios plist; imported outdent package; bumped version to 1.0.5

--- src/index.js
+++ src/index.js
@@ -6,12 +6,11 @@ import childProcess from 'child_process';
 import fileExists from 'file-exists';
 import fs from 'fs';
 import name from 'project-name';
+import outdent from 'outdent';
 import program from 'commander';
 import replace from 'replace';
 
-let projectName = name();
-
-projectName = projectName.replace(/\s/g, '');
+const projectName = name();
 
 const replaceOptions = {
 	recursive: true,
@@ -19,150 +18,178 @@ const replaceOptions = {
 };
 
 fs.readFile('./android/app/src/main/res/values/strings.xml', 'utf8', (err, markup) => {
-	if (err) return console.log(err);
 
-	const $ = cheerio.load(markup);
-	const appName = $('string[name=app_name]').text();
+	if (err === null) {
+		const $ = cheerio.load(markup);
+		const currentAppName = $('string[name=app_name]').text();
+		const nS_CurrentAppName = currentAppName.replace(/\s/g, '');
+		const lC_Ns_CurrentAppName = nS_CurrentAppName.toLowerCase();
 
-	program
-		.version('1.0.4')
-		.arguments('<currentProjectName> <newName>')
-		.action((currentProjectName, newName) => {
-			if (currentProjectName !== projectName) {
-				return console.log(`Project name didn't match!, your current project name is "${projectName}" `);
-			}
+		program
+			.version('1.0.5')
+			.arguments('<newName>')
+			.action((newName) => {
 
-			if (newName.replace(/\s/g, '') === projectName) {
-				return console.log(`Please try different name.`);
-			}
+				if (newName === currentAppName) {
+					return console.log(`Please try a different name.`);
+				}
 
-			const withSpaceNewName = newName;
-			newName = newName.replace(/\s/g, '');
-			const lowercaseCurrentProjectName = currentProjectName.toLowerCase();
-			const lowercaseNewName = newName.toLowerCase();
-			const pattern = /^([0-9]|[a-z])+([0-9a-z\s]+)$/i;
+				const nS_NewName = newName.replace(/\s/g, '');
+				const lC_Ns_NewName = nS_NewName.toLowerCase();
+				const pattern = /^([0-9]|[a-z])+([0-9a-z\s]+)$/i;
 
-			if (!pattern.test(newName)) {
-				return console.log(`"${newName}" is not a valid name for a project. Please use a valid identifier name (alphanumeric and space).`);
-			}
+				// Check if entered new name is valid based on regex pattern
+				if (!pattern.test(newName)) {
+					return console.log(`"${newName}" is not a valid name for a project. Please use a valid identifier name (alphanumeric and space).`);
+				}
 
-			// Android App name
-			replace({
-				regex: `<string name="app_name">${appName}</string>`,
-				replacement: `<string name="app_name">${withSpaceNewName}</string>`,
-				paths: [`./android/app/src/main/res/values/strings.xml`],
-				...replaceOptions
-			});
+				// Rename all folders
+				let commands = [
+					`mv ./ios/${nS_CurrentAppName} ios/${nS_NewName}`,
+					`mv ./ios/${nS_CurrentAppName}.xcodeproj/xcshareddata/xcschemes/${nS_CurrentAppName}.xcscheme ios/${nS_CurrentAppName}.xcodeproj/xcshareddata/xcschemes/${nS_NewName}.xcscheme`,
+					`mv ./ios/${nS_CurrentAppName}.xcodeproj ios/${nS_NewName}.xcodeproj`,
+					`mv ./ios/${nS_CurrentAppName}Tests/${nS_CurrentAppName}Tests.m ios/${nS_CurrentAppName}Tests/${nS_NewName}Tests.m`,
+					`mv ./ios/${nS_CurrentAppName}Tests ios/${nS_NewName}Tests`,
+					`mv ./android/app/src/main/java/com/${lC_Ns_CurrentAppName} android/app/src/main/java/com/${lC_Ns_NewName}`,
+					`rm -rf ./ios/${nS_CurrentAppName}`,
+					`rm -rf ./ios/${nS_CurrentAppName}.xcodeproj/xcshareddata/xcschemes/${nS_CurrentAppName}.xcscheme`,
+					`rm -rf ./ios/${nS_CurrentAppName}.xcodeproj`,
+					`rm -rf ./ios/${nS_CurrentAppName}Tests/${nS_CurrentAppName}Tests.m`,
+					`rm -rf ./ios/${nS_CurrentAppName}Tests`,
+					`rm -rf ./android/app/src/main/java/com/${lC_Ns_CurrentAppName}`,
+					`rm -rf ./ios/build`,
+					`rm -rf ./android/.gradle`,
+					`rm -rf ./android/app/build`,
+					`rm -rf ./android/build`
+				];
 
-			const iosPlistFiles = [
-				`./ios/${currentProjectName}/Info.plist`,
-				`./ios/${currentProjectName}Tests/Info.plist`
-			];
+				commands = commands.toString().replace(/,/g, ' && ');
 
-			// iOs App name
-			for (const file of iosPlistFiles) {
-				fs.readFile(file, 'utf8', (err, data) => {
-				    if (err) return console.log(err);
-
-				    const result = data.replace('<string>$(PRODUCT_NAME)</string>', `<string>${withSpaceNewName}</string>`);
-				    fs.writeFile(file, result, 'utf8', (err) => {
-				        if (err) return console.log(err);
-				    });
-				});
-			}
-
-			replace({
-				regex: `<string>${appName}</string>`,
-				replacement: `<string>${withSpaceNewName}</string>`,
-				paths: [
-					`./ios/${currentProjectName}/Info.plist`,
-					`./ios/${currentProjectName}Tests/Info.plist`
-				],
-				...replaceOptions
-			});
-
-			replace({
-				regex: currentProjectName,
-				replacement: withSpaceNewName,
-				paths: [`./ios/${currentProjectName}/Base.lproj/LaunchScreen.xib`],
-				...replaceOptions
-			});
-
-			replace({
-				regex: appName,
-				replacement: withSpaceNewName,
-				paths: [`./ios/${currentProjectName}/Base.lproj/LaunchScreen.xib`],
-				...replaceOptions
-			});
-
-			// Lowercase strings
-			replace({
-				regex: lowercaseCurrentProjectName,
-				replacement: lowercaseNewName,
-				paths: [
-					`./android/app/BUCK`,
-					`./android/app/build.gradle`,
-					`./android/app/src/main/AndroidManifest.xml`,
-					`./android/app/src/main/java/com/${lowercaseCurrentProjectName}/MainActivity.java`
-				],
-				...replaceOptions
-			});
-
-			if (fileExists(`./android/app/src/main/java/com/${lowercaseCurrentProjectName}/MainApplication.java`)) {
-				replace({
-					regex: lowercaseCurrentProjectName,
-					replacement: lowercaseNewName,
-					paths: [`./android/app/src/main/java/com/${lowercaseCurrentProjectName}/MainApplication.java`],
-					...replaceOptions
-				});
-			}
-
-			replace({
-				regex: currentProjectName,
-				replacement: newName,
-				paths: [
-					`./package.json`,
-					`./index.ios.js`,
-					`./ios/${currentProjectName}/AppDelegate.m`,
-					`./ios/${currentProjectName}.xcodeproj/xcshareddata/xcschemes/${currentProjectName}.xcscheme`,
-					`./ios/${currentProjectName}.xcodeproj/project.pbxproj`,
-					`./ios/${currentProjectName}Tests/${currentProjectName}Tests.m`,
-					`./index.android.js`,
-					`./android/app/src/main/java/com/${lowercaseCurrentProjectName}/MainActivity.java`,
-					`./android/settings.gradle`
-				],
-				...replaceOptions
-			});
-
-			let commands = [
-				`mv ./ios/${currentProjectName} ios/${newName}`,
-				`mv ./ios/${currentProjectName}.xcodeproj/xcshareddata/xcschemes/${currentProjectName}.xcscheme ios/${currentProjectName}.xcodeproj/xcshareddata/xcschemes/${newName}.xcscheme`,
-				`mv ./ios/${currentProjectName}.xcodeproj ios/${newName}.xcodeproj`,
-				`mv ./ios/${currentProjectName}Tests/${currentProjectName}Tests.m ios/${currentProjectName}Tests/${newName}Tests.m`,
-				`mv ./ios/${currentProjectName}Tests ios/${newName}Tests`,
-				`mv ./android/app/src/main/java/com/${lowercaseCurrentProjectName} android/app/src/main/java/com/${lowercaseNewName}`,
-				`rm -rf ./ios/${currentProjectName}`,
-				`rm -rf ./ios/${currentProjectName}.xcodeproj/xcshareddata/xcschemes/${currentProjectName}.xcscheme`,
-				`rm -rf ./ios/${currentProjectName}.xcodeproj`,
-				`rm -rf ./ios/${currentProjectName}Tests/${currentProjectName}Tests.m`,
-				`rm -rf ./ios/${currentProjectName}Tests`,
-				`rm -rf ./android/app/src/main/java/com/${lowercaseCurrentProjectName}`,
-				`rm -rf ./ios/build`,
-				`rm -rf ./android/.gradle`,
-				`rm -rf ./android/app/build`,
-				`rm -rf ./android/build`
-			];
-
-			commands = commands.toString().replace(/,/g, ' && ');
-
-			setTimeout(function () {
 				childProcess.exec(commands, (error, stdout) => {
-					if (error !== null) console.log(`exec error: ${error}`);
-					console.log(`App successfully renamed to "${withSpaceNewName}"`);
+					if (error !== null) {
+						console.log(outdent`
+							The app_name value in ./android/app/src/main/res/values/strings.xml must be the same with ./ios/AppFolderName before renaming the app.
+							ex. ./ios/CoolApp  --->  <string name="app_name">CoolApp</string>
+							and try running the command again with you new app name.
+							ex. react-native-rename "Awesome App"
+						`);
+						process.exit(1);
+					}
 				});
-			}, 1000);
-		})
-		.parse(process.argv);
 
-	if (!process.argv.slice(2).length) program.outputHelp();
+				setTimeout(function () {
+					// Replace android display name
+					replace({
+						regex: `<string name="app_name">${currentAppName}</string>`,
+						replacement: `<string name="app_name">${newName}</string>`,
+						paths: [`./android/app/src/main/res/values/strings.xml`],
+						...replaceOptions
+					});
+
+					// Replace ios display name
+					renameIosPlist(nS_NewName, newName);
+
+					// Replace LaunchScreen text for the first time
+					replace({
+						regex: `text="${nS_CurrentAppName}"`,
+						replacement: `text="${newName}"`,
+						paths: [`./ios/${nS_NewName}/Base.lproj/LaunchScreen.xib`],
+						...replaceOptions
+					});
+
+					// Replace LaunchScreen text
+					replace({
+						regex: `text="${currentAppName}"`,
+						replacement: `text="${newName}"`,
+						paths: [`./ios/${nS_NewName}/Base.lproj/LaunchScreen.xib`],
+						...replaceOptions
+					});
+
+					// Replace text based on newName w/o the spaces and in lowercase
+					replace({
+						regex: lC_Ns_CurrentAppName,
+						replacement: lC_Ns_NewName,
+						paths: [
+							`./android/app/BUCK`,
+							`./android/app/build.gradle`,
+							`./android/app/src/main/AndroidManifest.xml`,
+							`./android/app/src/main/java/com/${lC_Ns_NewName}/MainActivity.java`
+						],
+						...replaceOptions
+					});
+
+					// Check if MainApplication.java exists, because some project don't have this file
+					if (fileExists(`./android/app/src/main/java/com/${lC_Ns_NewName}/MainApplication.java`)) {
+						replace({
+							regex: lC_Ns_CurrentAppName,
+							replacement: lC_Ns_NewName,
+							paths: [`./android/app/src/main/java/com/${lC_Ns_NewName}/MainApplication.java`],
+							...replaceOptions
+						});
+					}
+
+					// Replace text based on newName w/o the spaces
+					replace({
+						regex: nS_CurrentAppName,
+						replacement: nS_NewName,
+						paths: [
+							`./index.ios.js`,
+							`./ios/${nS_NewName}/AppDelegate.m`,
+							`./ios/${nS_NewName}.xcodeproj/xcshareddata/xcschemes/${nS_NewName}.xcscheme`,
+							`./ios/${nS_NewName}.xcodeproj/project.pbxproj`,
+							`./ios/${nS_NewName}Tests/${nS_NewName}Tests.m`,
+							`./index.android.js`,
+							`./android/app/src/main/java/com/${lC_Ns_NewName}/MainActivity.java`,
+							`./android/settings.gradle`
+						],
+						...replaceOptions
+					});
+
+					// Replace package.json name property
+					replace({
+						regex: projectName,
+						replacement: nS_NewName,
+						paths: [
+							`./package.json`
+						],
+						...replaceOptions
+					});
+
+					console.log(`App successfully renamed to "${newName}"`);
+
+				}, 1000);
+
+			})
+			.parse(process.argv);
+
+		if (!process.argv.slice(2).length) program.outputHelp();
+
+	} else if (err.code === 'ENOENT') {
+		return console.log('Directory should be created using "react-native init"');
+	} else {
+		return console.log('Something went wrong: ', err.code);
+	}
+
 });
+
+function renameIosPlist(nS_NewName, newName) {
+	fs.readFile(`./ios/${nS_NewName}/Info.plist`, 'utf8', (err, markup) => {
+		const $ = cheerio.load(markup);
+		const CFBundleName = $("key:contains('CFBundleName')").next().text();
+		const iosPlistFiles = [
+			`./ios/${nS_NewName}/Info.plist`,
+			`./ios/${nS_NewName}Tests/Info.plist`
+		];
+
+		for (const file of iosPlistFiles) {
+			fs.readFile(file, 'utf8', (err, data) => {
+				if (err) return console.log(err);
+
+				const result = data.replace(CFBundleName, newName);
+				fs.writeFile(file, result, 'utf8', (err) => {
+					if (err) return console.log(err);
+				});
+			});
+		}
+	});
+}
