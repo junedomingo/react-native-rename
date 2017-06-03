254e650c831c5aacf07a351fa1158775109575af
June Domingo <junedomingo@gmail.com>
2017-06-03 13:07:45 +0800

    Update version to 2.0.3

--- src/index.js
+++ src/index.js
@@ -51,7 +51,7 @@ readFile('./android/app/src/main/res/values/strings.xml')
 		const lC_Ns_CurrentAppName = nS_CurrentAppName.toLowerCase();
 
 		program
-			.version('2.0.2')
+			.version('2.0.3')
 			.arguments('<newName>')
 			.action(newName => {
 				const nS_NewName = newName.replace(/\s/g, '');
