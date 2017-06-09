1c97ca422cb6f262239ed740cd706aab56282f05
Daniel van der Merwe <danieljmerwe@gmail.com>
2017-06-09 09:52:03 +0200

    Support for workspaces
    https://github.com/junedomingo/react-native-rename/issues/12

--- src/config/filesToModifyContent.js
+++ src/config/filesToModifyContent.js
@@ -31,6 +31,7 @@ export function filesToModifyContent(currentAppName, newName, projectName) {
 				'./index.android.js',
 				'./index.ios.js',
 				`./ios/${nS_NewName}.xcodeproj/project.pbxproj`,
+				`./ios/${nS_NewName}.xcworkspace/contents.xcworkspacedata`,
 				`./ios/${nS_NewName}.xcodeproj/xcshareddata/xcschemes/${nS_NewName}-tvOS.xcscheme`,
 				`./ios/${nS_NewName}.xcodeproj/xcshareddata/xcschemes/${nS_NewName}.xcscheme`,
 				`./ios/${nS_NewName}/AppDelegate.m`,
