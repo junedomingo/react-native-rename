db897ad05f1f97d7dd7ae3e6f8d806e7a9834961
June Domingo <junedomingo@gmail.com>
2017-11-05 01:12:07 +0800

    Add pretiier; update devDependencies

--- src/config/foldersAndFiles.js
+++ src/config/foldersAndFiles.js
@@ -4,7 +4,6 @@
 export function foldersAndFiles(currentAppName, newName) {
   const nS_CurrentAppName = currentAppName.replace(/\s/g, '');
   const nS_NewName = newName.replace(/\s/g, '');
-  const lC_Ns_CurrentAppName = nS_CurrentAppName.toLowerCase();
 
   return [
     `./ios/${nS_CurrentAppName}`,
@@ -16,6 +15,6 @@ export function foldersAndFiles(currentAppName, newName) {
     `./ios/${nS_NewName}.xcodeproj/xcshareddata/xcschemes/${nS_CurrentAppName}.xcscheme`,
     `./ios/${nS_CurrentAppName}Tests`,
     `./ios/${nS_NewName}Tests/${nS_CurrentAppName}Tests.m`,
-    `./ios/${nS_CurrentAppName}.xcworkspace`
+    `./ios/${nS_CurrentAppName}.xcworkspace`,
   ];
 }
--- src/index.js
+++ src/index.js
@@ -12,20 +12,22 @@ import pathExists from 'path-exists';
 import program from 'commander';
 import replace from 'node-replace';
 import shell from 'shelljs';
-import {foldersAndFiles} from './config/foldersAndFiles';
-import {filesToModifyContent} from './config/filesToModifyContent';
-import {bundleIdentifiers} from './config/bundleIdentifiers';
+import { foldersAndFiles } from './config/foldersAndFiles';
+import { filesToModifyContent } from './config/filesToModifyContent';
+import { bundleIdentifiers } from './config/bundleIdentifiers';
 
 const projectName = name();
 const replaceOptions = {
   recursive: true,
-  silent: true
+  silent: true,
 };
 
 function readFile(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, (err, data) => {
-      if (err) { reject(err); }
+      if (err) {
+        reject(err);
+      }
       resolve(data);
     });
   });
@@ -36,7 +38,7 @@ function replaceContent(regex, replacement, paths) {
     regex,
     replacement,
     paths,
-    ...replaceOptions
+    ...replaceOptions,
   });
 
   for (const path of paths) {
@@ -46,10 +48,17 @@ function replaceContent(regex, replacement, paths) {
 
 function moveJavaFiles(javaFiles, currentJavaPath, newBundlePath) {
   for (const file of javaFiles) {
-    mv(`${currentJavaPath}/${file}`, `${newBundlePath}/${file}`, {mkdirp: true}, err => {
-      if (err) return console.log('Error in moving java files.', err);
-      console.log(`${newBundlePath} ${colors.green('BUNDLE INDENTIFIER CHANGED')}`);
-    });
+    mv(
+      `${currentJavaPath}/${file}`,
+      `${newBundlePath}/${file}`,
+      {
+        mkdirp: true,
+      },
+      err => {
+        if (err) return console.log('Error in moving java files.', err);
+        console.log(`${newBundlePath} ${colors.green('BUNDLE INDENTIFIER CHANGED')}`);
+      }
+    );
   }
 }
 
@@ -63,8 +72,11 @@ readFile('./android/app/src/main/res/values/strings.xml')
     program
       .version('2.1.5')
       .arguments('<newName>')
-      .option('-b, --bundleID [value]', 'Set custom bundle identifier eg. "com.junedomingo.travelapp"')
-      .action((newName) => {
+      .option(
+        '-b, --bundleID [value]',
+        'Set custom bundle identifier eg. "com.junedomingo.travelapp"'
+      )
+      .action(newName => {
         const nS_NewName = newName.replace(/\s/g, '');
         const pattern = /^([0-9]|[a-z])+([0-9a-z\s]+)$/i;
         const lC_Ns_NewAppName = nS_NewName.toLowerCase();
@@ -74,16 +86,23 @@ readFile('./android/app/src/main/res/values/strings.xml')
         if (bundleID) {
           newBundlePath = bundleID.replace(/\./g, '/');
           const id = bundleID.split('.');
-          if (id.length < 2) return console.log('Invalid Bundle Identifier. Add something like "com.travelapp" or "com.junedomingo.travelapp"');
+          if (id.length < 2)
+            return console.log(
+              'Invalid Bundle Identifier. Add something like "com.travelapp" or "com.junedomingo.travelapp"'
+            );
         }
 
         if (!pattern.test(newName)) {
-          return console.log(`"${newName}" is not a valid name for a project. Please use a valid identifier name (alphanumeric and space).`);
+          return console.log(
+            `"${newName}" is not a valid name for a project. Please use a valid identifier name (alphanumeric and space).`
+          );
         }
 
-        if (newName === currentAppName
-          || newName === nS_CurrentAppName
-          || newName === lC_Ns_CurrentAppName) {
+        if (
+          newName === currentAppName ||
+          newName === nS_CurrentAppName ||
+          newName === lC_Ns_CurrentAppName
+        ) {
           return console.log(`Please try a different name.`);
         }
 
@@ -92,95 +111,95 @@ readFile('./android/app/src/main/res/values/strings.xml')
           './ios/build/*',
           './android/.gradle/*',
           './android/app/build/*',
-          './android/build/*'
+          './android/build/*',
         ]);
 
-        foldersAndFiles(currentAppName, newName)
-          .forEach((element, index) => {
-            const dest = element.replace(new RegExp(nS_CurrentAppName, 'gi'), nS_NewName);
-            pathExists(element)
-              .then(exists => {
-                setTimeout(() => {
-                  if (exists) {
-                    mv(element, dest, err => {
-                      if (err) return console.log('Error in renaming folder.', err);
-                      console.log(`${dest} ${colors.green('RENAMED')}`);
-                    });
-                  } else {
-                    // Rename children files and folders
-                    mv(element, dest, err => {
-                      if (err) return;
-                      console.log(`${dest} ${colors.green('RENAMED')}`);
-                    });
-                  }
-                }, 600*index);
-              });
+        foldersAndFiles(currentAppName, newName).forEach((element, index) => {
+          const dest = element.replace(new RegExp(nS_CurrentAppName, 'gi'), nS_NewName);
+          pathExists(element).then(exists => {
+            setTimeout(() => {
+              if (exists) {
+                mv(element, dest, err => {
+                  if (err) return console.log('Error in renaming folder.', err);
+                  console.log(`${dest} ${colors.green('RENAMED')}`);
+                });
+              } else {
+                // Rename children files and folders
+                mv(element, dest, err => {
+                  if (err) return;
+                  console.log(`${dest} ${colors.green('RENAMED')}`);
+                });
+              }
+            }, 600 * index);
           });
+        });
 
         setTimeout(() => {
           filesToModifyContent(currentAppName, newName, projectName).map(file => {
             file.paths.map((path, index) => {
               const newPaths = [];
-              pathExists(path)
-                .then(exists => {
-                  if (exists) {
-                    newPaths.push(path);
-                    setTimeout(() => {
-                      replaceContent(file.regex, file.replacement, newPaths);
-                    }, 500*index);
-                  }
-                });
+              pathExists(path).then(exists => {
+                if (exists) {
+                  newPaths.push(path);
+                  setTimeout(() => {
+                    replaceContent(file.regex, file.replacement, newPaths);
+                  }, 500 * index);
+                }
+              });
             });
           });
         }, 8000);
 
         setTimeout(() => {
-          readFile('./android/app/src/main/AndroidManifest.xml')
-            .then(data => {
-              const $ = cheerio.load(data);
-              const currentBundleID = $('manifest').attr('package');
-              const newBundleID = program.bundleID ? bundleID : `com.${lC_Ns_NewAppName}`;
-              const javaFileBase = './android/app/src/main/java';
-              const newJavaPath = `${javaFileBase}/${newBundleID.replace(/\./g, '/')}`;
-              const currentJavaPath = `${javaFileBase}/${currentBundleID.replace(/\./g, '/')}`;
+          readFile('./android/app/src/main/AndroidManifest.xml').then(data => {
+            const $ = cheerio.load(data);
+            const currentBundleID = $('manifest').attr('package');
+            const newBundleID = program.bundleID ? bundleID : `com.${lC_Ns_NewAppName}`;
+            const javaFileBase = './android/app/src/main/java';
+            const newJavaPath = `${javaFileBase}/${newBundleID.replace(/\./g, '/')}`;
+            const currentJavaPath = `${javaFileBase}/${currentBundleID.replace(/\./g, '/')}`;
 
-              const javaFiles = [
-                `MainActivity.java`,
-                `MainApplication.java`
-              ];
+            const javaFiles = [`MainActivity.java`, `MainApplication.java`];
 
-              if (bundleID) {
-                newBundlePath = newJavaPath;
-                moveJavaFiles(javaFiles, currentJavaPath, newBundlePath);
-              } else {
-                newBundlePath = newBundleID.replace(/\./g, '/').toLowerCase();
-                newBundlePath = `${javaFileBase}/${newBundlePath}`;
-                moveJavaFiles(javaFiles, currentJavaPath, newBundlePath);
-              }
+            if (bundleID) {
+              newBundlePath = newJavaPath;
+              moveJavaFiles(javaFiles, currentJavaPath, newBundlePath);
+            } else {
+              newBundlePath = newBundleID.replace(/\./g, '/').toLowerCase();
+              newBundlePath = `${javaFileBase}/${newBundlePath}`;
+              moveJavaFiles(javaFiles, currentJavaPath, newBundlePath);
+            }
 
-              setTimeout(function () {
-                bundleIdentifiers(currentAppName, newName, projectName, currentBundleID, newBundleID, newBundlePath).map(file => {
-                  file.paths.map((path, index) => {
-                    const newPaths = [];
-                    pathExists(path)
-                    .then(exists => {
-                      if (exists) {
-                        newPaths.push(path);
-                        setTimeout(() => {
-                          replaceContent(file.regex, file.replacement, newPaths);
-                        }, 500*index);
-                      }
-                    });
+            setTimeout(function() {
+              bundleIdentifiers(
+                currentAppName,
+                newName,
+                projectName,
+                currentBundleID,
+                newBundleID,
+                newBundlePath
+              ).map(file => {
+                file.paths.map((path, index) => {
+                  const newPaths = [];
+                  pathExists(path).then(exists => {
+                    if (exists) {
+                      newPaths.push(path);
+                      setTimeout(() => {
+                        replaceContent(file.regex, file.replacement, newPaths);
+                      }, 500 * index);
+                    }
                   });
                 });
-              }, 2000);
-            })
+              });
+            }, 2000);
+          });
         }, 10000);
-
-      }).parse(process.argv);
+      })
+      .parse(process.argv);
     if (!process.argv.slice(2).length) program.outputHelp();
   })
   .catch(err => {
-    if (err.code === 'ENOENT') return console.log('Directory should be created using "react-native init"');
+    if (err.code === 'ENOENT')
+      return console.log('Directory should be created using "react-native init"');
     return console.log('Something went wrong: ', err);
   });
