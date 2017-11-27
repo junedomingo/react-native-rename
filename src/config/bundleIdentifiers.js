3d37930c19bb2870cdb57892ba532a23bd99777b
June Domingo <junedomingo@gmail.com>
2017-11-27 22:51:39 +0800

    Clean paths

--- src/config/bundleIdentifiers.js
+++ src/config/bundleIdentifiers.js
@@ -1,14 +1,7 @@
 // nS - No Space
 // lC - Lowercase
 
-export function bundleIdentifiers(
-  currentAppName,
-  newName,
-  projectName,
-  currentBundleID,
-  newBundleID,
-  newBundlePath
-) {
+export function bundleIdentifiers(currentAppName, newName, projectName, currentBundleID, newBundleID, newBundlePath) {
   const nS_CurrentAppName = currentAppName.replace(/\s/g, '');
   const nS_NewName = newName.replace(/\s/g, '');
   const lC_Ns_CurrentBundleID = currentBundleID.toLowerCase();
@@ -18,11 +11,7 @@ export function bundleIdentifiers(
     {
       regex: currentBundleID,
       replacement: newBundleID,
-      paths: [
-        './android/app/BUCK',
-        './android/app/build.gradle',
-        './android/app/src/main/AndroidManifest.xml',
-      ],
+      paths: ['android/app/BUCK', 'android/app/build.gradle', 'android/app/src/main/AndroidManifest.xml'],
     },
     {
       regex: currentBundleID,
