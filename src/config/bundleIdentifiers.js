497744c7104d4d7dd3f82fca564f4322155aa838
vomchik <vomchik@gmail.com>
2020-12-05 19:36:01 +0200

    [android] Fix files deleting after change a bundle id

--- src/config/bundleIdentifiers.js
+++ src/config/bundleIdentifiers.js
@@ -4,8 +4,6 @@
 export function bundleIdentifiers(currentAppName, newName, projectName, currentBundleID, newBundleID, newBundlePath) {
   const nS_CurrentAppName = currentAppName.replace(/\s/g, '');
   const nS_NewName = newName.replace(/\s/g, '');
-  const lC_Ns_CurrentBundleID = currentBundleID.toLowerCase();
-  const lC_Ns_NewBundleID = newBundleID.toLowerCase();
 
   return [
     {
@@ -17,11 +15,6 @@ export function bundleIdentifiers(currentAppName, newName, projectName, currentB
       regex: currentBundleID,
       replacement: newBundleID,
       paths: [`${newBundlePath}/MainActivity.java`, `${newBundlePath}/MainApplication.java`],
-    },
-    {
-      regex: lC_Ns_CurrentBundleID,
-      replacement: lC_Ns_NewBundleID,
-      paths: [`${newBundlePath}/MainApplication.java`],
     },
     {
       // App name (probably) doesn't start with `.`, but the bundle ID will
